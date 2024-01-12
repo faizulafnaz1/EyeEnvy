685094be726a9be14e427b6f919f1baef62f9ddd
faizulafnaz1 <156345064+faizulafnaz1@users.noreply.github.com>
2024-01-12 11:30:45 +0530

    Update urls.py

--- EyeEnvy/urls.py
+++ EyeEnvy/urls.py
@@ -34,4 +34,4 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     path('offer/', include('offers.urls')),
 
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
